e2a162849b1a4ad245e8df9e345ae75cc01a93e9
Lonny154 <Lonny154@hotmail.com>
2025-04-04 10:54:58 -0500

    Add event listeners

--- Web/js/sketchPad.js
+++ Web/js/sketchPad.js
@@ -7,6 +7,20 @@ class SketchPad{
             background-color:white;
             box-shadow: 0px 0px 10px 2px black; 
         `;
-        container.appendChild(this.canvas)
+        container.appendChild(this.canvas);
+
+        this.ctx = this.canvas.getContext("2d");
+
+        this.#addEventListeners();
+
+    }
+    #addEventListeners(){
+        this.canvas.onmousedown=(evt)=>{
+            const rect = this.canvas.getBoundingClientRect();
+            const mouse = [
+                evt.clientX-rect.left,
+                evt.clientY-rect.top,
+            ];
+        }
     }
 }
